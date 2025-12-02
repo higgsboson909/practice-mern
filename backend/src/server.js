4b777a29ccdce1687f446c1140e41bef235c82e6
higgsboson909 <higgsbosson906@gmail.com>
2025-12-03 00:00:35 +0500

    Setup mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
 const app = express();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
   console.log("server started");
 });
+
+connectDB();
